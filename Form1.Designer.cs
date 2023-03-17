b5d000dddfb478d81320b5e4d5f7c9c4beb21797
Demonexe <demo.exe01@gmail.com>
2023-03-17 02:08:36 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RecruApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(266, 422);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(266, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(657, 422);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(24, 132);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(217, 64);
+            this.buttonInsert.TabIndex = 2;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.Location = new System.Drawing.Point(21, 55);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(76, 32);
+            this.labelName.TabIndex = 3;
+            this.labelName.Text = "Name";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 55);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 28);
+            this.textBox1.TabIndex = 4;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(24, 202);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(217, 64);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(24, 272);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(217, 64);
+            this.buttonUpdate.TabIndex = 6;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 422);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.splitter1);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace RecruApp
+{
+    public partial class MainWindow : Form
+    {
+        public static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = RecruAppDb; Integrated Security = True; Connect Timeout = 30; Encrypt=False";
+        public DataTable dgvDataSource;
+        private int ?row;
+        string clickedString;
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateDGV();
+        }
+
+        private void UpdateDGV()
+        {
+            string sqlQuery = "Select * from [dbo].[Components]";
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                dgvDataSource = new DataTable();
+                sqlConn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = sqlConn;
+                cmd.CommandText = sqlQuery;
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+                sqlDataAdapter.SelectCommand = cmd;
+                sqlDataAdapter.Fill(dgvDataSource);
+                dataGridView1.DataSource = dgvDataSource;
+                sqlDataAdapter.Dispose();
+                cmd.Dispose();
+                sqlConn.Close();
+            }
+            row = null;
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            string sqlQuery = $"Insert into [dbo].[Components] values(NEXT VALUE FOR [dbo].[Components_Seq], '{textBox1.Text}')"; // change 10 flat value to 
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = sqlConn;
+                cmd.CommandText = sqlQuery;
+                cmd.ExecuteNonQuery().ToString();
+                cmd.Dispose();
+                sqlConn.Close();
+            }
+            UpdateDGV();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (row == null)
+                return;
+            string sqlQuery = $"Delete from [dbo].[Components] where Name = '{textBox1.Text}'";
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = sqlConn;
+                cmd.CommandText = sqlQuery;
+                cmd.ExecuteNonQuery().ToString();
+                cmd.Dispose();
+                sqlConn.Close();
+            }
+            UpdateDGV();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (row == null)
+                return;
+            string sqlQuery = $"Update [dbo].[Components] set Name = '{textBox1.Text}' where Name = '{clickedString}'";
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = sqlConn;
+                cmd.CommandText = sqlQuery;
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                sqlConn.Close();
+            }
+            UpdateDGV();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            row = e.RowIndex;
+            clickedString = dgvDataSource.Rows[e.RowIndex].Field<string>(1);
+            textBox1.Text = clickedString;
+        }
+    }
+}
